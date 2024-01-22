abb11a3d567e3398e1fa141ce4c688b7f6c9b833
Tvesha Shah <tveshashah13@gmail.com>
2024-01-22 09:03:13 -0500

    demo_0

---
+++ src/main/java/ca/mcscert/se2aa4/demos/tennis/Match.java
@@ -0,0 +1,12 @@
+package ca.mcscert.se2aa4.demos.tennis;
+
+public class Match{
+    public Match(Integer p1Strength, Integer p2Strength){
+
+
+    }
+
+    public String play(){
+        return " No winner... yet";
+    }
+}
