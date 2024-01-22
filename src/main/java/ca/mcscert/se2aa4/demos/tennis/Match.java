7aca5fb28e902b6cc8863ae7c4d34b36ec7eb347
Tvesha Shah <tveshashah13@gmail.com>
2024-01-22 09:19:55 -0500

    names

--- src/main/java/ca/mcscert/se2aa4/demos/tennis/Match.java
+++ src/main/java/ca/mcscert/se2aa4/demos/tennis/Match.java
@@ -1,8 +1,12 @@
 package ca.mcscert.se2aa4.demos.tennis;
 
 public class Match{
-    public Match(Integer p1Strength, Integer p2Strength){
 
+    public static final String P1_name = "p1";
+    
+    public static final String P2_name = "p2";
+
+    public Match(Integer p1Strength, Integer p2Strength){
 
     }
 
